716b8bbb06d44387cfa0a19ddc334327aa32fc95
Ann <ann@Anns-MacBook-Pro.local>
2024-04-29 10:25:25 -0600

    Actualizar url

--- web/src/app/shared/constants/api.url.ts
+++ web/src/app/shared/constants/api.url.ts
@@ -2,7 +2,7 @@
 export const URL_WEB = "http://localhost:4200/";
 
 //REST Service Configuration
-export const URL_HOST = "http://localhost:8080/";
+export const URL_HOST = "http://35.232.184.195:8080/";
 
 export const PATH_DOCUMENTO = "documento";
 export const PATH_DOCUMENTO_FILTERS = "/geyByFilters";
